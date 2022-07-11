dedc5326de65ee6c8a4d1b830757535a81a25f26
TATA-YOYO <4learnhacking@free.fr>
2022-07-11 17:37:50 +0200

    I added a test

--- src/test/java/com/safetynet/alerts/ServiceTest.java
+++ src/test/java/com/safetynet/alerts/ServiceTest.java
@@ -2,6 +2,7 @@ package com.safetynet.alerts;
 
 import com.safetynet.alerts.controller.dto.PersonDto;
 import com.safetynet.alerts.services.ServiceAPI;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,25 +11,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class ServiceTest {
-
+    private List<String> addressList = new ArrayList<>();
     @Autowired
     private ServiceAPI serviceAPI;
 
-    @Test
-    void getPersonToShareList() {
-        //ARRANGE
-        List<String> addressList = new ArrayList<>();
+    @BeforeEach
+    public void setings() {
+        addressList.add("1509 Culver St");
         addressList.add("834 Binoc Ave");
         addressList.add("748 Townings Dr");
         addressList.add("112 Steppes Pl");
-        addressList.add("1509 Culver St");
+        addressList.add("748 Townings Dr");
+
+
+
+    }
+
+
+    @Test
+    void getPersonToShareList() {
         //Act
-List<PersonDto> personDtoList = serviceAPI.getPersonToShareList(addressList);
+        List<PersonDto> personDtoList = serviceAPI.getPersonToShareList(addressList);
         //Assert
-        assertEquals(11,personDtoList.size() );
+        assertEquals(13, personDtoList.size());
+    }
+
+    @Test
+    void getAddressTest() {
+        //Act
+        List<String> address = serviceAPI.getAddress(3);
+        //Assert
+        assertTrue(address.equals(addressList));
     }
 
 }
